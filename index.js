d1f642aee06994ee99f3942c7345967465fb8e49
Anubhav23106 <dilkhush2a1@gmail.com>
2025-03-24 18:51:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let text=document.querySelector(".text p")
+
+text.innerHTML = text.innerHTML.split("").map((char,i)=>
+    `<b style="transform:rotate(${i * 6.3}deg")>${char}</b>`).join("");
+
+
+
+ScrollReveal({
+
+    distance:"90px", duration:2000,delay:200,
+});
+
+ScrollReveal().reveal(".hero",{origin:"top"});
